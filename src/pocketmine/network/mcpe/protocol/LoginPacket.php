c10db2026b51fa8bfe6aad88d6478676b10f50c8
xandoofc <iouvagnerjoaca@gmail.com>
2026-05-15 07:54:21 -0300

    fix: skip JWT chain verification for protocol 944+ (skipVerification=true)

--- src/pocketmine/network/mcpe/protocol/LoginPacket.php
+++ src/pocketmine/network/mcpe/protocol/LoginPacket.php
@@ -138,6 +138,8 @@ class LoginPacket extends DataPacket
 		}
 
 		if($this->protocol >= 900 && isset($this->authInfo["Token"]) && is_string($this->authInfo["Token"])){
+			$this->chainData = ["chain" => [$this->authInfo["Token"]]];
+			$this->skipVerification = true;
 			$token = $this->authInfo["Token"];
 			$parts = explode(".", $token);
 			if(isset($parts[1])){
