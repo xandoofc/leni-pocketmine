5c01071c44072060b51987b3506ebd19cca9747a
xandoofc <iouvagnerjoaca@gmail.com>
2026-05-16 07:57:03 -0300

    Reordered handshake packets and disabled creative content for 975

---
+++ scratch/test_nbt_empty.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use pocketmine\nbt\NetworkLittleEndianNBTStream;
+use pocketmine\nbt\tag\CompoundTag;
+
+$nbt = new CompoundTag("");
+
+$stream = new NetworkLittleEndianNBTStream();
+$buffer = $stream->write($nbt);
+
+echo "Length: " . strlen($buffer) . "\n";
+echo "Hex: " . bin2hex($buffer) . "\n";
